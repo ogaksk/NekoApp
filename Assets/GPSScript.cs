d3639d430b0df7c344891ecc5d5e44420387e00a
ogawakeisuke <ogaksk@ogaksk.local>
2017-09-04 22:07:17 +0900

    init

---
+++ Assets/GPSScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPSScript : MonoBehaviour {
+
+	// Use this for initialization
+	// void Start () {	
+	// }
+    public string text = "text";
+    private GUIStyle m_guiStyle;
+
+    IEnumerator Start() 
+    {
+        m_guiStyle = new GUIStyle();
+        m_guiStyle.fontSize = 30;   // フォントサイズの変更.
+
+        Debug.Log("start");
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            print("no service.");
+            text = "no service.";
+            yield break;
+        }
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            text = "Timed out";
+            print("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            text = "Unable to determine device location";
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            text  = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI () 
+    {
+         GUI.Label(new Rect(20, 40, 80, 20), text, m_guiStyle);
+    }
+}
+
---
+++ Assets/HandleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HandleScript : MonoBehaviour {
+  public Text MousePos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     if (Input.GetMouseButton(0))
+    {
+      Vector3 screenPos = Input.mousePosition;
+      Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Camera.main.transform.forward * 1);
+      transform.position = worldPos;
+
+      MousePos.text = screenPos.ToString();
+    }
+	}
+
+}
